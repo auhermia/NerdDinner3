e3e5b57e4bc979a04cf752e33cc03711624a4d84
Hermia Au <au.hermia@gmail.com>
2017-10-20 02:54:25 -0700

    Add project files.

---
+++ NerdDinner3/Controllers/DinnersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NerdDinner3.Models;
+
+// empty controller
+
+namespace NerdDinner3.Controllers
+{
+    public class DinnersController : Controller
+    {
+        // HTTP-GET: /Dinners/
+        public void Index()
+        {
+            var dinners = DinnerRepository.FindUpcomingDinners().ToList();
+        }
+
+        // HTTP-GET: /Dinners/Details/2
+
+        public void Details(int id)
+        {
+            Dinner dinner = DinnerRepository.GetDinner(id);
+        }
+    }
+}
---
+++ NerdDinner3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NerdDinner3.Startup))]
+namespace NerdDinner3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
