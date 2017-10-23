d9f945618a8fb16940d73aa52f0facb9d92e2b69
Hermia Au <au.hermia@gmail.com>
2017-10-23 12:27:25 -0700

    10/22/17

--- NerdDinner3/Controllers/DinnersController.cs
+++ NerdDinner3/Controllers/DinnersController.cs
@@ -11,17 +11,86 @@ namespace NerdDinner3.Controllers
 {
     public class DinnersController : Controller
     {
+        DinnerRepository dinnerRepository = new DinnerRepository();
+
         // HTTP-GET: /Dinners/
-        public void Index()
+                public ActionResult Index()
         {
-            var dinners = DinnerRepository.FindUpcomingDinners().ToList();
+            var dinners = dinnerRepository.FindUpcomingDinners().ToList();
+
+            return View(dinners);
         }
 
-        // HTTP-GET: /Dinners/Details/2
+        // HTTP-GET: /Dinners/Details/#
 
-        public void Details(int id)
+        public ActionResult Details(int id)
         {
-            Dinner dinner = DinnerRepository.GetDinner(id);
+            Dinner dinner = dinnerRepository.GetDinner(id);
+
+            if (dinner == null)
+                return View("NotFound");
+            else
+                return View(dinner);
         }
+
+
+
+        // GET: /Dinners/Edit/# --------------------
+
+        
+        // Retrieving Form Post Values
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Edit(int id, FormCollection formValues)
+        {
+            // Retrieve existing dinner
+            Dinner dinner = dinnerRepository.GetDinner(id);
+
+            try
+            {
+                UpdateModel(dinner);
+
+                // Persist changes back to db
+                dinnerRepository.Save();
+
+                // Perform HTTP redirect to details page for the saved Dinner
+                return RedirectToAction("Details", new { id = dinner.DinnerID });
+            }
+            
+            catch
+            {
+                ModelState.AddRuleViolations(dinner.GetRuleValidations());
+                return View(dinner);
+            }
+
+        }
+        // keep or nah? - where does this go?
+        public static class ControllerHelpers
+        {
+
+            public static void AddRuleViolations(this ModelStateDictionary modelState, IEnumerable<RuleViolation> errors)
+            {
+
+                foreach (RuleViolation issue in errors)
+                {
+                    modelState.AddModelError(issue.PropertyName, issue.ErrorMessage);
+                }
+            }
+        }
+
+        //
+        // GET: /Dinners/Create
+
+        public ActionResult Create()
+        {
+            Dinner dinner = new Dinner()
+            {
+                EventDate = DateTime.Now.AddDays(7)
+            };
+
+            return View(dinner);
+        }
+
+
     }
 }
